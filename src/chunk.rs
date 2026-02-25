9f9c895cc8f9b273d0df619162d5637c61520876
Finn Bear <finntbear@gmail.com>
2026-02-24 20:31:17 -0800

    Delinearize.

--- src/chunk.rs
+++ src/chunk.rs
@@ -8,8 +8,15 @@ pub const PADDED: usize = CHUNK_SIZE + 2 * PADDING; // 18
 pub const PADDED_VOLUME: usize = PADDED * PADDED * PADDED;
 
 #[inline]
-pub fn padded_idx(pos: UVec3) -> usize {
-    pos.x as usize + pos.y as usize * PADDED + pos.z as usize * PADDED * PADDED
+pub fn linearize(padded_pos: UVec3) -> usize {
+    padded_pos.x as usize + padded_pos.y as usize * PADDED + padded_pos.z as usize * PADDED * PADDED
+}
+
+#[inline]
+pub fn delinearize(index: usize) -> UVec3 {
+    let i = index as u32;
+    const P: u32 = PADDING as u32;
+    UVec3::new(i % P, (i / P) % P, i / (P * P))
 }
 
 pub struct PaddedChunk<B: Block> {
@@ -26,16 +33,16 @@ impl<B: Block> PaddedChunk<B> {
     #[inline]
     pub fn set(&mut self, pos: UVec3, block: B) {
         let p = PADDING as u32;
-        self.data[padded_idx(pos + UVec3::splat(p))] = block;
+        self.data[linearize(pos + UVec3::splat(p))] = block;
     }
 
     #[inline]
     pub fn set_padded(&mut self, pos: UVec3, block: B) {
-        self.data[padded_idx(pos)] = block;
+        self.data[linearize(pos)] = block;
     }
 
     #[inline]
     pub fn get_padded(&self, pos: UVec3) -> &B {
-        &self.data[padded_idx(pos)]
+        &self.data[linearize(pos)]
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ mod mesh;
 pub use block::{
     Block, CrossInfo, CrossStretch, CullMode, Shape, SlabInfo, Thickness, FULL_THICKNESS,
 };
-pub use chunk::{padded_idx, PaddedChunk, CHUNK_SIZE, PADDED, PADDED_VOLUME, PADDING};
+pub use chunk::{delinearize, linearize, PaddedChunk, CHUNK_SIZE, PADDED, PADDED_VOLUME, PADDING};
 pub use face::{Axis, DiagonalFace, Face, QuadFace};
 pub use mesh::{block_faces, block_faces_into, greedy_mesh, greedy_mesh_into, Quad, Quads};
